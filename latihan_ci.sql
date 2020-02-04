cd00a4022e8c0dfe32d3cc5893bf14ae326f4323
Yoga Afdilla <yogaafdilla.j@gmail.com>
2020-02-04 19:24:11 +0700

    DB update

--- latihan_ci.sql
+++ latihan_ci.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 30, 2019 at 02:00 PM
+-- Generation Time: Feb 04, 2020 at 01:22 PM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 5.6.36
 
@@ -40,8 +40,26 @@ CREATE TABLE `data_login` (
 
 INSERT INTO `data_login` (`id`, `username`, `password`) VALUES
 (1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997'),
-(2, 'yoga', '6b3bebb6d418f3726781486e710abee36f1b371b'),
-(3, 'jamal', 'e0195770807aa8c82b0b128d9c0423b5ad035172');
+(2, 'yoga', '6b3bebb6d418f3726781486e710abee36f1b371b');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_login_user`
+--
+
+CREATE TABLE `data_login_user` (
+  `int_npm` int(12) NOT NULL,
+  `password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `data_login_user`
+--
+
+INSERT INTO `data_login_user` (`int_npm`, `password`) VALUES
+(17111156, 'da39a3ee5e6b4b0d3255bfef95601890afd80709'),
+(17111111, 'da39a3ee5e6b4b0d3255bfef95601890afd80709');
 
 -- --------------------------------------------------------
 
@@ -53,6 +71,8 @@ CREATE TABLE `data_mahasiswa` (
   `id` int(11) NOT NULL,
   `int_npm` int(12) NOT NULL,
   `str_nama` varchar(100) NOT NULL,
+  `str_prodi` varchar(30) NOT NULL,
+  `str_kelas` varchar(20) NOT NULL,
   `int_semester` int(2) NOT NULL,
   `gambar` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -61,12 +81,15 @@ CREATE TABLE `data_mahasiswa` (
 -- Dumping data for table `data_mahasiswa`
 --
 
-INSERT INTO `data_mahasiswa` (`id`, `int_npm`, `str_nama`, `int_semester`, `gambar`) VALUES
-(1, 17111156, 'Yoga Afdilla Jamaluddin', 5, 'https___blogs-images_forbes_com_ryanwhitwam_files_2017_12_arena-of-valor1.jpg'),
-(2, 17111111, 'Dendi ', 4, 'wp3276889-pubg-4k-wallpapers1.jpg'),
-(3, 17111112, 'Reynaldi', 3, 'Logo-avengers-wallpaper-HD-pictures-download.jpg'),
-(4, 17111113, 'Naufal', 2, 'gov_gaming___Bo6IZfmAZT9___.jpg'),
-(5, 17111114, 'Denar', 1, 'bumidatar.jpg');
+INSERT INTO `data_mahasiswa` (`id`, `int_npm`, `str_nama`, `str_prodi`, `str_kelas`, `int_semester`, `gambar`) VALUES
+(1, 17111156, 'Yoga Afdilla Jamaluddin', 'Teknik Informatika', 'TIF RP 17 CID C', 5, 'Logo-avengers-wallpaper-HD-pictures-download2.jpg'),
+(22, 17111111, 'Andri', 'Teknik Informatika', 'TIF RP 18 CID A', 6, 'Logo-avengers-wallpaper-HD-pictures-download3.jpg'),
+(23, 17111112, 'Reynaldi', 'Teknik Informatika', 'TIF RP 16 B', 4, 'Logo-avengers-wallpaper-HD-pictures-download4.jpg'),
+(24, 17111113, 'Anggi', 'Teknik Industri', 'TI RP 17 A', 3, 'Logo-avengers-wallpaper-HD-pictures-download1.jpg'),
+(25, 17111114, 'Naufal', 'Desain Komunikasi Visual', 'DKV RM 17 A', 5, 'Logo-avengers-wallpaper-HD-pictures-download5.jpg'),
+(26, 17111115, 'Dendi ', 'Teknik Industri', 'TI WK 15 B', 3, 'Logo-avengers-wallpaper-HD-pictures-download6.jpg'),
+(27, 17111116, 'Nazira', 'Teknik Informatika', 'TIF RM 17 B', 5, 'Logo-avengers-wallpaper-HD-pictures-download7.jpg'),
+(28, 17111117, 'Denar', 'Teknik Informatika', 'TIF WK 18 C', 6, 'Logo-avengers-wallpaper-HD-pictures-download8.jpg');
 
 --
 -- Indexes for dumped tables
@@ -92,13 +115,13 @@ ALTER TABLE `data_mahasiswa`
 -- AUTO_INCREMENT for table `data_login`
 --
 ALTER TABLE `data_login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `data_mahasiswa`
 --
 ALTER TABLE `data_mahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
